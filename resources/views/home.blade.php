298b2c4e5986d5cc03bcf4958ea2f66040d7be35
Jyoti Gupta <jyoti_gupta@seologistics.com>
2019-02-07 11:00:34 +0530

    add current date in dashobaord page

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -10,7 +10,9 @@
 @endsection
 @section('content')
     <div class="panel panel-default">
-        <div class="panel-heading dashHd">Dashboard</div>
+        <div class="panel-heading clearfix dashHd">Dashboard   
+         <span class="rgt"><h4>Current Date -  @php echo date("d.m.Y  h:i:s"); @endphp </h4></span>
+     </div>
 
         <div class="panel-body">
             @if (session('status'))
