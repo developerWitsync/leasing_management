f182cd78bde2c98a73dd8ef29211940b4ffa0f6c
Himanshu Rajput <himanshu_rajput@seologistics.com>
2019-04-24 17:44:25 +0530

    changes for the export of the interest and depreciation

--- app/Exports/InterestAndDepreciationExport.php
+++ app/Exports/InterestAndDepreciationExport.php
@@ -15,19 +15,14 @@ class InterestAndDepreciationExport implements FromView
         $this->asset_id = $id;
     }
 
+    /**
+     * generates the excel for the interest and depreciation...
+     * @return View
+     */
     public function view(): View
     {
         $interest_depreciation = InterestAndDepreciation::query()->where('asset_id', '=', $this->asset_id)->get();
         $interest_depreciation = collect($interest_depreciation)->groupBy('modify_id');
-
         return view('leasevaluation.partials._interest_and_depreciation', compact('interest_depreciation'));
-    }
-
-    /**
-    * @return \Illuminate\Support\Collection
-    */
-    public function collection()
-    {
-        return InterestAndDepreciation::query()->where('asset_id', '=', $this->asset_id)->get();
     }
 }
