8c552636734622bde9ead287263b666fd9dc14a2
Himanshu Rajput <himanshu_rajput@seologistics.com>
2019-05-09 19:04:22 +0530

    reverted

--- app/Http/Controllers/Lease/ReviewSubmitController.php
+++ app/Http/Controllers/Lease/ReviewSubmitController.php
@@ -358,8 +358,7 @@ class ReviewSubmitController extends Controller
             } else {
                 $previous_depreciation = $this->calculateDepreciation($subsequent_modify_required, $asset, $previous_liability);
                 $previous_accumulated_depreciation = 0;
-                //$previous_carrying_value_of_lease_asset = $previous_liability;
-                $previous_carrying_value_of_lease_asset = $asset->value_of_lease_asset;
+                $previous_carrying_value_of_lease_asset = $previous_liability;
                 $value_of_lease_asset = $previous_carrying_value_of_lease_asset;
             }
 
