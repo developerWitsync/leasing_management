63dcf4426786fa60374860d000859ffdf2e7b949
Himanshu Rajput <himanshu_rajput@seologistics.com>
2019-05-21 10:32:58 +0530

    changes for the scroll issue

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -299,7 +299,6 @@
     <!-- Modal -->
     <div id="policiesModal" class="modal fade" role="dialog">
         <div class="modal-dialog  modal-lg">
-
             <!-- Modal content-->
             <div class="modal-content">
                 <div class="modal-header privacypopBg">
