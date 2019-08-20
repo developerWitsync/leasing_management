beb0f8d2dd8593bb85e6075716e2db13edc486e0
developer@witsync.co <developer@witsync.co>
2019-08-20 20:24:52 +0530

    rates to be uploaded in chunks

--- app/Http/Controllers/Settings/CurrenciesController.php
+++ app/Http/Controllers/Settings/CurrenciesController.php
@@ -477,20 +477,27 @@ class CurrenciesController extends Controller
           ->where('id', '=', $id)
           ->firstOrFail();
         $final_data = [];
-        foreach ($data as $datum){
-          $internal = [];
-          $internal['foreign_currency_id'] = $model->id;
-          $internal['date'] = $datum[0];
-          $internal['rate'] = $datum[3];
-          $internal['created_at'] = date('Y-m-d H:i:s');
-          $internal['updated_at'] = date('Y-m-d H:i:s');
-          $final_data[] = $internal;
-        }
 
-        DB::transaction(function () use ($final_data, $id) {
+        DB::transaction(function () use ($final_data, $id, $data, $model) {
           DB::table('exchange_rates')->where('foreign_currency_id', '=', $id)->delete();
-          DB::table('exchange_rates')->insert($final_data);
+          foreach (array_chunk($data, 2000) as $chunk){
+            foreach ($chunk as $datum){
+              $internal = [];
+              $internal['foreign_currency_id'] = $model->id;
+              $internal['date'] = $datum[0];
+              $internal['rate'] = $datum[3];
+              $internal['created_at'] = date('Y-m-d H:i:s');
+              $internal['updated_at'] = date('Y-m-d H:i:s');
+              $final_data[] = $internal;
+            }
+            DB::table('exchange_rates')->insert($final_data);
+          }
         });
+
+//        DB::transaction(function () use ($final_data, $id) {
+//          DB::table('exchange_rates')->where('foreign_currency_id', '=', $id)->delete();
+//          DB::table('exchange_rates')->insert($final_data);
+//        });
 
         return redirect()->back()->with('status', 'Exchange rates have been saved successfully.');
 
@@ -498,6 +505,7 @@ class CurrenciesController extends Controller
         return redirect()->back()->with('error', 'Please choose the xlsx file from which the exchange rates will be imported. You can first download the template for the same by clicking on the "Download Excel for Importing" button.');
       }
     } catch (\Exception $e) {
+      dd($e);
       abort(404);
     }
   }
